e23f3485c6a375ab1eae5a8dca13589b55871a82
rakeshshah18 <21amtics464@gmail.com>
2024-09-30 23:17:53 +0530

    update sheet link

--- src/components/ExcelUploader.jsx
+++ src/components/ExcelUploader.jsx
@@ -8,7 +8,7 @@ const ExcelDataDisplay = () => {
     useEffect(() => {
         // Function to fetch data from the API
         const fetchDataFromAPI = async () => {
-            const apiURL = 'https://script.googleusercontent.com/macros/echo?user_content_key=hwZP5bKhqTybcBUiPM93womA1swm2IyauJP1WPhIDDGn832s1Tz5JivvGUAPMoBvOo9f3HE1_N4zKMckdCxSRlHiuzuzhBpnm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnPw6a5xi5BrU4sMAO-NN35L_0cO6BAUJ9KbhG82rVwsHsQI3rxflJnGeJwpBhaGuFgeshIrtnjc0dAGDt_3fAgYGJS9lv9AGgQ&lib=MLTuqJQ6NeB5boqzk7sJ1GX33-3kBXtAK'; // Replace with your actual API endpoint
+            const apiURL = 'https://script.google.com/macros/s/AKfycbxUVuAQHncuJNGJVFj3UwzmaTOmryr4Aj6Qxv_UPXLHvptMpKKU5nHR2nSA3R8ON34g/exec'; // Replace with your actual API endpoint
             try {
                 const response = await fetch(apiURL);
                 if (!response.ok) {
